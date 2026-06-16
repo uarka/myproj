f7986c59ad391709fc8dca3ffd681374f136f56a
Felicity Janet Meadows <root@felicitymeadows.com>
2026-06-16 16:07:48 +0100

    Called the func my_func in hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+static int b;
+
+extern int my_func(int a)
 {
+     int c = b;
+     c+=1;
+     c+=a;
+     return c;
+}
+
+extern int main(int argc, char *argv[])
+{
+    b = 4;
+    printf("%d\n", my_func(2));
     printf("Hello, World from here!\n");
     return 0;
 }
